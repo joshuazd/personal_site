b7ce1894f97675db7f51056f4457487bca2c0d2d
joshuazd <zinkdudajoshua@gmail.com>
2025-09-02 11:21:36 -0500

    Add resume download

---
+++ app/controllers/resume_controller.rb
@@ -0,0 +1,14 @@
+class ResumeController < ApplicationController
+  def download
+    pdf_path = Rails.root.join("tmp/resume.pdf")
+
+    unless File.exist?(pdf_path)
+      system("bundle exec rails 'pdf:generate[false]'")
+    end
+
+    send_file pdf_path,
+              type: "application/pdf",
+              disposition: "attachment",
+              filename: "JoshuaZinkDuda_Resume.pdf"
+  end
+end
